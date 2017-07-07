7939e666d66686f038341e2387183b608880bd69
lin jiang <ll-jiangjiang@hotmail.com>
2017-07-07 10:49:30 +0800

    后端实现： 1. 查看活动、修改活动、修改活动协议 2. 房屋信息列表、房屋总价修改

--- apt/urls.py
+++ apt/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
-from .views import EventListView,EventCreateView
+from .views import (EventListView, EventCreateView, EventUpdateView,
+                    EventDetailView,
+                    EventDetailListView, EventDetailTotalUpdateView,
+                    EventTermUpdateView)
 
 urlpatterns = [
     url(r'^list/', EventListView.as_view(), name='event_list'),
     url(r'^create/', EventCreateView.as_view(), name='event_create'),
+    url(r'^(?P<pk>\d+)/', EventDetailView.as_view(), name='event_detail'),
+    url(r'^update/(?P<pk>\d+)/$', EventUpdateView.as_view(), name='event_update'),
+    url(r'^update/(?P<pk>\d+)/term/', EventTermUpdateView.as_view(), name='event_term_update'),
 
+
+    url(r'^room/list/', EventDetailListView.as_view(), name='room_list'),
+    url(r'^room/(?P<pk>\d+)/price/', EventDetailTotalUpdateView.as_view(), name='room_price_update')
 ]
--- apt/views.py
+++ apt/views.py
@@ -1,14 +1,11 @@
-
-from django.views.generic import ListView
-from django.views.generic.edit import CreateView
-from .models import Event
-# from .form import EventCreateForm
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Event, EventDetail
 
 
 class EventListView(ListView):
     template_name = 'event_list.html'
     model = Event
-    # paginate_by = 12
 
     def get_queryset(self):
         return self.model.objects.order_by('-id')
@@ -18,5 +15,37 @@ class EventCreateView(CreateView):
     model = Event
     fields = [f.name for f in model._meta.get_fields()]
     template_name = 'event_create.html'
-    # form_class = EventCreateForm
 
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'popup/event_detail.html'
+
+
+class EventUpdateView(UpdateView):
+    model = Event
+    fields = [f.name for f in model._meta.get_fields()]
+    template_name = 'event_create.html'
+
+
+class EventTermUpdateView(UpdateView):
+    model = Event
+    fields = ['term']
+    template_name = 'popup/event_term.html'
+
+
+class EventDetailListView(ListView):
+    template_name = 'eventdetail_list.html'
+    model = EventDetail
+
+    def get_queryset(self):
+        return self.model.objects.order_by('-id')
+
+
+class EventDetailTotalUpdateView(UpdateView):
+    '''
+    修改线上总价
+    '''
+    template_name = 'popup/eventdetail_total.html'
+    fields = ['total']
+    model = EventDetail
