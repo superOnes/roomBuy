bffe97f2ea90e3923cf28f1a283ae90183800b9f
lin jiang <linjiang@lindeMacBook-Pro.local>
2017-07-12 10:09:00 +0800

    发布活动整合

--- static/customJS/index.js
+++ static/customJS/index.js
@@ -44,27 +44,6 @@ function roomPriceFile(thisID){
 	});
 };
 
-
-//取消发布
-function Unpublish(thisID){
-	$.ajax({
-		type:"POST",
-		url:"../pubstatus/",
-		async:true,
-		data:{id:thisID},
-		cache: false,
-        processData: false,//发送的数据将被转换为对象，false就是不转化，默认为true
-        contentType: false,
-		success:function(){
-			window.location.reload();
-		},
-		error:function(){
-			alert("未知错误")
-		}
-	});
-};
-
-
 //清除公测名单\\\ 删除认筹名单
 function deleteOrder(thisID,url){
 	$.ajax({
@@ -86,6 +65,6 @@ function deleteOrder(thisID,url){
 
 function onkeypress(event) {
     var x = event.which || event.keyCode;
-    
-    
+
+
 }
