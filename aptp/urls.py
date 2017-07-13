8ea385d39341ad474feb930b0ad0ef9bd82bf690
lin jiang <ll-jiangjiang@hotmail.com>
2017-07-13 13:55:47 +0800

    1.新增移动端成功预订页面 2.新增移动端订单列表页面 3.新增移动端订单详情页面 4.新增移动端协议页面

--- aptp/urls.py
+++ aptp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from .views import (AppLoginView, AppEventDetailView, AppEventDetailListView,
-                    AppEventDetailDetailView,
-                    HomePageView, HouseDetailView)
+                    AppEventDetailDetailView, AppHouseSuccessView,
+                    AppOrderListView, AppOrderInfoView)
 
 urlpatterns = [
     url(r'^login/', AppLoginView.as_view(), name='app_login'),
@@ -9,8 +9,12 @@ urlpatterns = [
         name='app_eventdetail'),
     url(r'^(?P<pk>\d+)/houses/', AppEventDetailListView.as_view(),
         name='app_house_list'),
-    url(r'^house/(?P<pk>\d+)/', AppEventDetailDetailView.as_view(),
+    url(r'^house/(?P<pk>\d+)/$', AppEventDetailDetailView.as_view(),
         name='app_house_info'),
-    url(r'^homepage/', HomePageView.as_view(), name='home_page_view'),
-    url(r'^housedel/', HouseDetailView.as_view(), name='aptp_eventdel'),
+    url(r'^house/(?P<pk>\d+)/success/', AppHouseSuccessView.as_view(),
+        name='app_house_success'),
+    url(r'^customer/(?P<pk>\d+)/orders', AppOrderListView.as_view(),
+        name='app_order_list'),
+    url(r'^order/(?P<pk>\d+)', AppOrderInfoView.as_view(),
+        name='app_order_info'),
 ]
--- aptp/views.py
+++ aptp/views.py
@@ -35,57 +35,13 @@ class AppEventDetailDetailView(TemplateView):
         return context
 
 
-class HomePageView(TemplateView):
-    '''
-    登录之后主页面
-    '''
-    template_name = 'homepage.html'
-
-    def get_context_data(self, request, **kwargs):
-        context = super(HomePageView, self).get_context_data()
-        customer = request.user.username
-        timenow = timezone.localtime(
-            timezone.now()).strftime("%Y-%m-%d %H:%M:%S")
-        objs = Event.objects.values(
-            'name',
-            'phone_num',
-            'test_start',
-            'test_end',
-            'event_start',
-            'event_end',
-            'cover').filter(
-            event_end__gte=timenow).filter(is_pub=True)
-        context['customer'] = customer
-        context['events'] = objs
-        return context
+class AppHouseSuccessView(TemplateView):
+    template_name = 'aptp/house_success.html'
 
 
-class HouseDetailView(TemplateView):
-    '''
-    房子具体信息
-    '''
-    template_name = 'eventdel.html'
-
-    def get_context_data(self, request, **kwargs):
-        context = super(HouseDetailView, self).get_context_data()
-        id = request.GET.get('id')  # 房子id号
-
-        obj = EventDetail.objects.values(
-            'batch',
-            'house_type',
-            'floor',
-            'floor_area',
-            'price',
-            'is_sold').get(id)
-        visit=EventDetail.get(id=id)
-        visit.visit_num=visit.visit_num+1
-        visit.save()
-        # 还缺少房子平面图
-        context['eventdetail'] = obj
-        return context
+class AppOrderListView(TemplateView):
+    template_name = 'aptp/order_list.html'
 
 
-# class ListView(TemplateView):
-#     template_name = 'eventdellist.html'
-#     def get_context_data(self, **kwargs):
-#         context=super(ListView,self).get_context_data()
+class AppOrderInfoView(TemplateView):
+    template_name = 'aptp/order_info.html'
