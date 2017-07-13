75f0c4656943a4655633d9293da283142a77799f
hexiaofeng <hexiaofeng@onest.net>
2017-07-13 15:16:22 +0800

    搜索功能、导入文件问题、发布上架提示消息修改

--- static/customJS/index.js
+++ static/customJS/index.js
@@ -1,49 +1,4 @@
 
-//上传认筹名单文件
-function submitFile(thisID){
-	var files = $("#files")[0].files;
-	var data = new FormData(); //转化为表单格式的数据
-    data.append('filename', files[0]);
-    data.append('id', thisID);
-	$.ajax({
-		type:"POST",
-		url:"/acc/import/",
-		async:true,
-		data:data,
-		cache: false,
-        processData: false,//发送的数据将被转换为对象，false就是不转化，默认为true
-        contentType: false,
-		success:function(){
-			window.location.reload();
-		},
-		error:function(){
-			alert("未知错误")
-		}
-	});
-};
-//导入房价文件
-function roomPriceFile(thisID){
-	var files = $("#priceFile")[0].files;
-	var data = new FormData(); //转化为表单格式的数据
-    data.append('file', files[0]);
-    data.append('id', thisID);
-	$.ajax({
-		type:"POST",
-		url:"/event/importprice/",
-		async:true,
-		data:data,
-		cache: false,
-        processData: false,//发送的数据将被转换为对象，false就是不转化，默认为true
-        contentType: false,
-		success:function(){
-			window.location.reload();
-		},
-		error:function(){
-			alert("未知错误");
-		}
-	});
-};
-
 //清除公测名单
 function deleteOrder(thisID,url){
 	$.ajax({
@@ -62,4 +17,3 @@ function deleteOrder(thisID,url){
 		}
 	});
 };
-
