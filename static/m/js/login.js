52865ad32dddd2ab685e6f088937b5ecf42f9b37
yanglingqian <13691592204@163.com>
2017-07-27 11:18:37 +0800

    苹果手机兼容性

--- static/m/js/login.js
+++ static/m/js/login.js
@@ -247,10 +247,6 @@ function updataTime(data) {
 		var date4 = new Date($(".test_ent").html()).getTime();//公测
 		var date5 = date2 - date;  //时间差的毫秒数
 		var date6 = date4 - date;
-		console.log(data.event_start);
-		console.log($(".event_end").html());
-		console.log($(".test_start").html());
-		console.log($(".test_ent").html());
 		//计算出相差天数
 		if(date<date3){
 			$('#endTimeing').html("公测活动未开始");
