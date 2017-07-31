b9e0ceba094217e9ce9ec223d7d01cf5e02ff9b5
yanglingqian <13691592204@163.com>
2017-07-31 10:36:37 +0800

    移动端调整

--- static/m/js/login.js
+++ static/m/js/login.js
@@ -1,6 +1,6 @@
 var userid;
-//var http="";//正式
-var http="http://10.7.10.193:8000"; //测试
+var http="";//正式
+//var http="http://10.7.10.193:8000"; //测试
 //var http="http://10.7.1.34";
 $(function(){
 
