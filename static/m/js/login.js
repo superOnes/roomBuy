963ee08fcb9faac65c38dcda1478c916724de4ee
yanglingqian <13691592204@163.com>
2017-08-04 14:36:40 +0800

    去掉alert显示的url

--- static/m/js/login.js
+++ static/m/js/login.js
@@ -84,6 +84,14 @@ function prol(data){
 						'<p>'+data.term+'</p>'
 	));
 }
+window.alert = function(name){
+    var iframe = document.createElement("IFRAME");
+    iframe.style.display="none";
+    iframe.setAttribute("src", 'data:text/plain,');
+    document.documentElement.appendChild(iframe);
+    window.frames[0].window.alert(name);
+    iframe.parentNode.removeChild(iframe);
+}
 
 /*首页创建元素*/
 function creatEle(data){
