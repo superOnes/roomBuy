648c89113d31d3b12b60769149556b01e04845cf
chenlijc123 <18332040609@163.com>
2017-08-06 20:09:12 +0800

    修改导入认筹名单电话号码限制

--- accounts/views.py
+++ accounts/views.py
@@ -188,7 +188,13 @@ class ImportView(View):
                     value2 = sheet.cell(rowx=rx, colx=1).value
                     value3 = sheet.cell(rowx=rx, colx=2).value
                     value4 = sheet.cell(rowx=rx, colx=3).value
-                    if type(value1) == str and type(value2) == float and type(value3) == str and type(value4) == str:
+
+                    if type(value1) == str and type(value3) == str and type(value4) == str:
+                        if type(value2) == float:
+                            try:
+                                value2 = str(int(value2))
+                            except:
+                                return JsonResponse({'response_state': 400, 'msg': '导入手机号格式有误！'})
                         li.append(value1)
                         li.append(value2)
                         li.append(value3)
