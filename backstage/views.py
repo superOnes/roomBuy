00b8cc7a9578cb59756462eb80a5da2cdbce9385
hexiaofeng <hexiaofeng@onest.net>
2017-08-15 14:24:59 +0800

    用户列表页搭建

--- backstage/views.py
+++ backstage/views.py
@@ -9,6 +9,7 @@ class LoginView(View):
     登录
     '''
     def get(self, request):
+<<<<<<< HEAD
         return render(request, 'BMS/login.html')
 
 
@@ -18,3 +19,6 @@ class HomeListView(ListView):
 
     def get_queryset(self):
         return self.model.objects.filter(is_admin=True)
+=======
+        return render(request, 'bms/login.html')
+>>>>>>> two
