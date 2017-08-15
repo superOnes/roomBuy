5dbd95dc43971328204b8743a1e6300c880c61b9
hexiaofeng <hexiaofeng@onest.net>
2017-08-15 15:58:51 +0800

    解决冲突

--- backstage/urls.py
+++ backstage/urls.py
@@ -4,12 +4,7 @@ from django.conf.urls import url
 from backstage.views import LoginView, HomeListView,CreateView
 
 urlpatterns = [
-<<<<<<< HEAD
-    url(r'^login/$', LoginView.as_view(), name='bms_login'),
-    url(r'^$', HomeListView.as_view(), name='bms_home'),
-=======
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^userlist/$', HomeListView.as_view(), name='home'),
     url(r'^createuser/$', CreateView.as_view(), name='createuser'),
->>>>>>> twp
 ]
