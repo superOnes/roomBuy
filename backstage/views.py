6f12ea03196a09d075633dec1a6f6a9bc0ab1824
hexiaofeng <hexiaofeng@onest.net>
2017-08-15 14:27:39 +0800

    解决路径冲突

--- backstage/views.py
+++ backstage/views.py
@@ -9,16 +9,12 @@ class LoginView(View):
     登录
     '''
     def get(self, request):
-<<<<<<< HEAD
-        return render(request, 'BMS/login.html')
+        return render(request, 'bms/login.html')
 
 
 class HomeListView(ListView):
-    template_name = 'BMS/home.html'
+    template_name = 'bms/home.html'
     model = User
 
     def get_queryset(self):
         return self.model.objects.filter(is_admin=True)
-=======
-        return render(request, 'bms/login.html')
->>>>>>> two
