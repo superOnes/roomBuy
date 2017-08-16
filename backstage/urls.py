47fa5e25e687d272f3944e2d307825e62dc0c0e8
chenlijc123 <18332040609@163.com>
2017-08-16 15:11:45 +0800

    创建后台用户

--- backstage/urls.py
+++ backstage/urls.py
@@ -2,12 +2,14 @@
 from django.conf.urls import url
 
 from backstage.views import LoginView, HomeListView, CreateView,\
-    GetProvinceView, GetCityView
+    GetProvinceView, GetCityView, DeleteUserView, ModifyUserView
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^userlist/$', HomeListView.as_view(), name='home'),
     url(r'^createuser/$', CreateView.as_view(), name='createuser'),
     url(r'^getprovince/$', GetProvinceView.as_view(), name='get_province'),
-    url(r'^getcity/$', GetCityView.as_view(), name='get_city')
+    url(r'^getcity/$', GetCityView.as_view(), name='get_city'),
+    url(r'^modify/$', ModifyUserView.as_view(), name='modify'),
+    url(r'^delete/$', DeleteUserView.as_view(), name='delete')
 ]
--- backstage/views.py
+++ backstage/views.py
@@ -1,4 +1,5 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, request
+from django.http import QueryDict
 from django.shortcuts import render, redirect
 from django.views.generic import View, ListView
 from django.contrib import messages
@@ -6,6 +7,7 @@ from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 
 from accounts.models import User
+from apt.models import Company
 from backstage.models import Province, City
 
 
@@ -44,9 +46,71 @@ class CreateView(View):
         return render(request, 'bms/createuser.html')
 
     def post(self, request):
-        name = request.POST.get('username')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        name = request.POST.get('name')
         house_limit = request.POST.get('house_limit')
         expire_date = request.POST.get('expire_date')
+        province = request.POST.get('province')
+        city = request.POST.get('city')
+        print(username, password, name, house_limit, expire_date, province, city)
+        if username is None or password is None or name is None \
+                or province is None or city is None:
+            return JsonResponse({'successs': False})
+        filteruser = User.objects.filter(username=username)
+        if filteruser:
+            return JsonResponse({'success': False, 'msg': '用户名已存在'})
+        else:
+            company = Company.objects.create(name=name,
+                                             house_limit=house_limit,
+                                             expire_date=expire_date
+                                             if expire_date else '',
+                                             province_id=province,
+                                             city_id=city)
+            User.objects.create_user(
+                username=username,
+                password=password,
+                is_admin=True,
+                company=company)
+            return JsonResponse({'success': True, 'msg': '创建成功！'})
+
+
+class ModifyUserView(View):
+    '''
+    修改用户信息
+    '''
+    def put(self, requests, *args, **kwargs):
+        put = QueryDict(request.body, encoding=request.encoding)
+        id = put.get('id')
+        username = put.get('username')
+        password = put.get('password')
+        name = put.get('name')
+        house_limit = put.get('house_limit')
+        expire_date = put.get('expire_date')
+        province = put.get('province')
+        city = put.get('city')
+        if id:
+            user = User.get(id)
+            company = user.company
+            user.username = username
+            user.password = password
+            company.name = name
+            company.house_limit = house_limit
+            company.expire_date = expire_date
+            company.province = province
+            company.city = city
+            return JsonResponse({'success': True})
+        return JsonResponse({'success': False})
+
+
+class DeleteUserView(View):
+        '''
+        删除账户
+        '''
+        def delete(self, request):
+            params = QueryDict(request.body, encoding=request.encoding)
+            User.remove(params.get('id'))
+            return JsonResponse({'success': True})
 
 
 class GetProvinceView(View):
